d276d835e8d4585e24d0c9e5d117a49779fa8535
Varvara <varya.sheptunova@gmail.com>
2023-01-30 13:21:29 +0400

    Task 47. Create an array

---
+++ Task47/Program.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+
+WriteLine("Введите размерность матрицы, минимальное и максимальное значение её элементов через пробел: ");
+string[] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+double[,] array = GetMatrixArray(int.Parse(parameters[0]), int.Parse(parameters[1]), double.Parse(parameters[2]), double.Parse(parameters[3]));
+PrintMatrixArray(array);
+
+double[,] GetMatrixArray(int rows, int columns, double minValue, double maxValue)
+{
+    double[,] resultMatrixArray = new double[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            resultMatrixArray[i,j] = rnd.NextDouble() * (maxValue - minValue) + minValue;
+        }
+    }
+    return resultMatrixArray;
+}
+
+void PrintMatrixArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j].ToString("F2")} ");
+        }
+        WriteLine();
+    }
+}
